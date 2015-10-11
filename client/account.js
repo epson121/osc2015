22570a086bb06185b57da883c947b97177dfa06d
Stanislav Mihic <stamba@Stanislavs-MacBook-Pro.local>
2015-10-11 02:31:38 +0200

    another commit

--- client/account.js
+++ client/account.js
@@ -63,3 +63,21 @@ Template.header.helpers({
       }
     }
   });
+
+toastr.options = {
+  "closeButton": false,
+  "debug": true,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
